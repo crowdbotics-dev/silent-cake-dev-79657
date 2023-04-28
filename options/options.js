d60ba3c50d16c8eab5c1326c79716ea35cde8771
Crowdbotics <engineering@crowdbotics.com>
2023-04-28 09:47:51 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "silent-cake-dev-79657",
+  url: "https://silent-cake-dev-79657.botics.co",
+  api: "https://silent-cake-dev-79657.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
